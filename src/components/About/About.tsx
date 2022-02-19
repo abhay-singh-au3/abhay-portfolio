dd4a9014dfd7501bafaf1d31dd2913f2293c8ef8
abhay-singh-au3 <abhaysingh2402@gmail.com>
2022-02-19 16:05:33 +0530

    adding bio

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -17,16 +17,16 @@ export function About() {
                     <h2>About me</h2>
                 </ScrollAnimation>
                 <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000}>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius dolor iure tempora placeat ab in ut, pariatur magni, repellat debitis voluptatibus iusto. Nihil, excepturi. Vitae blanditiis natus aliquid adipisci.</p>
+                    <p>Experienced software engineer based in mumbai with a background in building and integrating Loyalty Management System and ChatBots for various brands. </p>
                 </ScrollAnimation>
-                <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+                {/* <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
 
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dicta error ut numquam possimus eius dolorum voluptate ad nam assumenda minima, sint rem, quos blanditiis expedita quasi distinctio! Natus, non!</p>
                 </ScrollAnimation>
                 <ScrollAnimation animateIn="fadeInLeft" delay={0.6 * 1000}>
 
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam nemo quasi, ipsam minus asperiores at doloremque magni labore, perspiciatis impedit dolorum ab atque mollitia excepturi animi nisi deserunt dicta expedita?</p>
-                </ScrollAnimation>
+                </ScrollAnimation> */}
 
                 <ScrollAnimation animateIn="fadeInLeft" delay={0.7 * 1000}>
                     <h3>Here are my main skills:</h3>
